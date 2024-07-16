a0059741d7d9949226e0744fb5ca70ccbdf7d609
kaizhou.chen <chenkaizhou@outlook.com>
2024-07-16 17:53:38 +0800

    优化vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,12 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path, { resolve } from "path";
 import fs from 'fs';
-import dayjs from 'dayjs'
 
 import AutoImport from 'unplugin-auto-import/vite'
 import monacoEditorPluginModule from 'vite-plugin-monaco-editor'
+import dayjs from 'dayjs'
 
-const isObjectWithDefaultFunction = (module: unknown): module is { default: typeof monacoEditorPluginModule } => (
-  module != null &&
-  typeof module === 'object' &&
-  'default' in module &&
-  typeof module.default === 'function'
-)
-const monacoEditorPlugin = isObjectWithDefaultFunction(monacoEditorPluginModule)
-  ? monacoEditorPluginModule.default
-  : monacoEditorPluginModule
-
-const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
-const DRIVE_LETTER_REGEX = /^[a-z]:/i;
-
+const monacoEditorPlugin = getMonacoEditorPlugin();
 const timestamp = dayjs(new Date()).format('YYYY-MM-DD_HH.mm.ss')
 
 // https://vitejs.dev/config/
@@ -43,17 +31,7 @@ export default defineConfig(({ mode }) => {
       }),
 
       // 自定义插件
-      {
-        name: 'copy-gitignore',
-        writeBundle() {
-          // 如果是部署到 github，则覆盖 .gitignore，删除掉里面的 dist 目录
-          if (process.argv.includes('--deploy')) {
-            const src = path.resolve(__dirname, '.bak/.gitignore');
-            const dest = path.resolve(__dirname, '.gitignore');
-            fs.copyFile(src, dest, () => {})
-          }
-        }
-      }
+      githubPlugin()
     ],
 
     // 配置别名 @
@@ -80,30 +58,18 @@ export default defineConfig(({ mode }) => {
       rollupOptions: {
         // 打包时，将多个入口，分开打包
         input: {
-          main: resolve(__dirname, 'index.html'),
+          index: resolve(__dirname, 'index.html'),
           login: resolve(__dirname, 'login.html')
         },
         
         output: {
-          // 问题 1：由于浏览器的缓存机制，每次部署后，都要强制刷新
-          // 解决方法：给文件添加时间戳，每次部署后，都是新文件，需要重新下载
+          // 文件名添加时间戳，避免缓存
           entryFileNames: `assets/[name].[hash].${timestamp}.js`,
           chunkFileNames: `assets/[name].[hash].${timestamp}.js`,
           assetFileNames: `assets/[name].[hash].${timestamp}.[ext]`,
 
-
-          // 问题 2：部署到github后，_plugin-vue_export-helper.js 访问不到
-          // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
-          sanitizeFileName(name) {
-            const match = DRIVE_LETTER_REGEX.exec(name);
-            const driveLetter = match ? match[0] : "";
-            // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
-            // Otherwise, avoid them because they can refer to NTFS alternate data streams.
-            return (
-              driveLetter +
-              name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
-            );
-          },
+          // 处理文件名里的特殊符号
+          sanitizeFileName,
         },
       }
     },
@@ -121,3 +87,56 @@ export default defineConfig(({ mode }) => {
     }
   }
 })
+
+/**
+ * 部署到 github 时，覆盖 .gitignore，删除掉里面的 dist 目录。
+ * 因为 dist 目录要提交到 gh-pages 分支，以便部署到 Github Pages
+ */
+function githubPlugin() {
+  const gitignore = '# Logs\nlogs\n*.log\nnpm-debug.log*\nyarn-debug.log*\nyarn-error.log*\npnpm-debug.log*\nlerna-debug.log*\n\nnode_modules\n.DS_Store\n*.local\n\n# Editor directories and files\n.idea\n*.suo\n*.ntvs*\n*.njsproj\n*.sln\n*.sw?'
+
+  return {
+    name: 'github-plugin',
+    writeBundle() {
+      // 
+      if (process.argv.includes('--deploy')) {
+        const file = path.resolve(__dirname, '.gitignore');
+        fs.writeFileSync(file, gitignore, 'utf8');
+      }
+    }
+  }
+}
+
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
+// 解决问题：部署到github后，_plugin-vue_export-helper.js 访问不到
+// https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+function sanitizeFileName(name: any) {
+  const match = DRIVE_LETTER_REGEX.exec(name);
+  const driveLetter = match ? match[0] : "";
+  // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+  // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+  return (
+    driveLetter +
+    name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+  );
+}
+
+/**
+ * 获取 monacoEditorPlugin
+ */
+function getMonacoEditorPlugin() {
+  const isObjectWithDefaultFunction = (module: unknown): module is { default: typeof monacoEditorPluginModule } => (
+    module != null &&
+    typeof module === 'object' &&
+    'default' in module &&
+    typeof module.default === 'function'
+  )
+
+  const monacoEditorPlugin = isObjectWithDefaultFunction(monacoEditorPluginModule)
+    ? monacoEditorPluginModule.default
+    : monacoEditorPluginModule;
+
+  return monacoEditorPlugin;
+}
